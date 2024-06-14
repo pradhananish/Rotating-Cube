5c9f4488732e48ca4b257b1261e003c5ab135cfd
pradhananish <101039304+pradhananish@users.noreply.github.com>
2024-06-15 00:07:37 +0545

    rotating cube

---
+++ rotatingcube/Color.py
@@ -0,0 +1,91 @@
+import pygame
+from pygame.locals import *
+
+from OpenGL.GL import *
+from OpenGL.GLU import *
+
+import random
+
+vertices = (
+    (1, -1, -1),
+    (1, 1, -1),
+    (-1, 1, -1),
+    (-1, -1, -1),
+    (1, -1, 1),
+    (1, 1, 1),
+    (-1, -1, 1),
+    (-1, 1, 1)
+)
+
+edges = (
+    (0, 1),
+    (1, 2),
+    (2, 3),
+    (3, 0),
+    (4, 5),
+    (5, 6),
+    (6, 7),
+    (7, 4),
+    (0, 4),
+    (1, 5),
+    (2, 6),
+    (3, 7)
+)
+
+surfaces = (
+    (0, 1, 2, 3),
+    (3, 2, 7, 6),
+    (6, 7, 5, 4),
+    (4, 5, 1, 0),
+    (1, 5, 7, 2),
+    (4, 0, 3, 6)
+)
+
+colors = (
+    (0, 1, 1),  # Cyan
+    (1, 1, 1),  # White
+    (0, 1, 1),  # Cyan
+    (0, 1, 1),  # Cyan
+    (0.5, 0, 0.5),  # Lighter Purple
+    (0, 1, 1),  # Cyan
+    (0, 0, 1),  # Blue
+    (1, 1, 1),  # White
+    (0, 1, 1),  # Cyan
+    (0.5, 0, 0.5)  # Lighter Purple
+)
+
+def Cube():
+    glBegin(GL_QUADS)
+    for surface in surfaces:
+        for i, vertex in enumerate(surface):
+            glColor3fv(colors[i % len(colors)])
+            glVertex3fv(vertices[vertex])
+    glEnd()
+
+    glBegin(GL_LINES)
+    for edge in edges:
+        for vertex in edge:
+            glVertex3fv(vertices[vertex])
+    glEnd()
+
+def main():
+    pygame.init()
+    display = (800, 600)
+    pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
+    gluPerspective(45, (display[0] / display[1]), 0.1, 50.0)
+    glTranslatef(0.0, 0.0, -5)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return
+
+        glRotatef(1, 3, 1, 1)
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        Cube()
+        pygame.display.flip()
+        pygame.time.wait(10)
+
+if __name__ == "__main__":
+    main()
